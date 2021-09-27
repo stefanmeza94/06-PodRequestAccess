2d5743877ab9a62eefbfd1d12ec34bb15fe7283f
Stefan Meza <stefanmeza93@gmail.com>
2021-09-27 12:56:25 +0200

    done

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 const btn = document.querySelector('.btn_access');
-const input = document.getElementById('email');
-const error = document.querySelector('.error_message');
+const input = document.querySelector('.email_input');
+const error = document.querySelector('.error');
 
 function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
-
-console.log(error.text)
-
-// console.log(validateEmail('stefamezgmail.com'));
 
 function showErrorMessage(message) {
   error.textContent = message;
@@ -20,7 +16,12 @@ btn.addEventListener('click', function() {
   if (input.value === '') {
     showErrorMessage('Oops! Please add your email!');
   } else if (!validateEmail(input.value)) {
-    showErrorMessage('Oops! Please check your email');
+    showErrorMessage('Oops! Please check your email!');
   }
-  
+});
+
+document.body.addEventListener('click', function(e) {
+  if (e.target !== input && e.target !== btn) {
+    error.style.visibility = 'hidden';
+  }
 });
